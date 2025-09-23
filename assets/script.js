02a2cf5693285209b06add4952d0ea585553933b
aleishis <mecedesval@gmail.com>
2025-09-23 11:22:30 -0700

    Se agrega archivo JavaScript

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// Evento que se dispara cuando se termina de cargar el DOM
+// DOM: Document Object Model
+document.addEventListener("DOMContentLoaded", function(){
+
+    // Declaramos una variable en JS que representa el "btn-ok" en el HTML
+    var btnOk = document.getElementById("btn-ok");
+
+    // Declaramos un evento "click" a ese boton
+    btnOk.addEventListener("click", function(){
+        // Declaramos una variable que representa el input del HTML
+        var name = document.getElementById("input-name");
+        
+        // Mostamos alerta del navegador con el contenido del input
+        alert("Hello world " + name.value);
+    })
+    
+})
