e9e7a79943f44608fcddbb0d69f0a538bbb403f7
aleishis <mecedesval@gmail.com>
2025-10-02 07:43:32 -0700

    Se agrega boton para dividir en math.js

--- assets/math.js
+++ assets/math.js
@@ -3,10 +3,10 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     // Declaramos una variable en JS que representa el "btn-ok" en el HTML
-    var btnOk = document.getElementById("btn-sumar");
+    var btnSumar = document.getElementById("btn-sumar");
 
     // Declaramos un evento "click" a ese boton
-    btnOk.addEventListener("click", function(){
+    btnSumar.addEventListener("click", function(){
         // Declaramos una variable que representa el input del HTML
         var value1Number = parseFloat(document.getElementById("input-value1").value);
         
@@ -58,6 +58,25 @@ document.addEventListener("DOMContentLoaded", function(){
         }
 
         var resultado = value1Number * value2Number
+        // Mostamos alerta del navegador con el contenido del input
+        alert("El resultado es: " + resultado)
+    })
+
+    var btnDividir = document.getElementById("btn-dividir");
+
+    btnDividir.addEventListener("click", function(){
+        // Declaramos una variable que representa el input del HTML
+        var value1Number = parseFloat(document.getElementById("input-value1").value);
+        
+        var value2Number = parseFloat(document.getElementById("input-value2").value)
+
+
+        // isNaN = is not a number
+        if (isNaN(value1Number) || isNaN(value2Number)){
+            alert("Estas estupido")
+        }
+
+        var resultado = value1Number / value2Number
         // Mostamos alerta del navegador con el contenido del input
         alert("El resultado es: " + resultado)
     })
